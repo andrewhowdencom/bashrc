cd1149008598b1f748b996ade452b2b4bab459a7
Andrew Howden <andrew.howden@fontis.com.au>
2015-03-11 18:20:25 +1100

    Make ssh verbose by default

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
 # Aliases
 alias psgr="ps aux | grep -i" # Search running processes for string
 alias cpp="rsync --progress " # Copy with progress bar, using RSYNC
+alias ssh="ssh -v "           # Make ssh more talkative by default
