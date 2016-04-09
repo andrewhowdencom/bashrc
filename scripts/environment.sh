4024aa7de499d451a5d3328872bedcaa2b6ea08b
Andrew Howden <hello@andrewhowden.com>
2016-04-09 15:01:15 +1000

    Rework structure

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,9 +1,9 @@
 # Uncomment whichever is appropriate
 # ENVIRONMENT_TYPE=production
-# ENVIRONMENT_TYPE=user
+ ENVIRONMENT_TYPE=user
 # ENVIRONMENT_TYPE=staging
 # ENVIRONMENT_TYPE=development
- ENVIRONMENT_TYPE=disposable        # Designated for VM's that are spun up to mess around with something
+# ENVIRONMENT_TYPE=disposable        # Designated for VM's that are spun up to mess around with something
 
 # ENVIRONMENT_PUSHOVER_USER=void
 # ENVIRONMENT_PUSHOVER_APP=void
