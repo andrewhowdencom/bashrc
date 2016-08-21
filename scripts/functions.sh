6e22c66ebf3eb9b3fdbd4f5ef60aed060048f150
Andrew Howden <hello@andrewhowden.com>
2016-08-21 18:22:56 +1000

    Remove redundant functions from bashrc, add new function to wrap xclip

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,33 +1,3 @@
-# Add the vendor/bin to the terminal path. 
-function add-path-composer {
-    # Test if a vendor path exists 
-    # That causes problems with multiple autoloads. Doesn't work at the 
-    # moment as there is a vendor in the ~/.composer/vendor/bin path
-    # if [[ $PATH == *"vendor"* ]]; then 
-    #    echo "Vendor already in path";
-    #    return 1;
-    # fi
-
-    # Add the vendor path
-    VENDOR_PATH="$(pwd)/vendor/bin"
-    if [[ -e "$VENDOR_PATH" ]]; then
-        export PATH=$PATH:$VENDOR_PATH
-        echo "Added $VENDOR_PATH to \$PATH"
-    else
-        echo "Cannot find vendor path"
-        return 1
-    fi
-}
-
-# Function to clear caches locally, save a couple of lines. Also makes sure
-# we don't accidently run the same command on live
-function clear-caches {
-    redis-cli flushall
-    sudo service php5-fpm restart
-    echo "Cleared redis, restarted php5-fpm"
-}
-
-
 # Run a mysql query against the local host
 function mysql-query {
     ERR_MSG="There is no \$%s environment variable set. Set one to continue"
@@ -40,27 +10,6 @@ function mysql-query {
    done
 
    mysql --host="$MYSQL_SERVER" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --database="$MYSQL_DATABASE" -e "$1"
-}
-
-# Function to test whether a PHP file will run successfully via cron as
-# a given user
-function php-cron {
-   if [[ -z $1 ]]; then 
-     echo "Usage: $0 {user} {path}"
-     return 1 
-   fi
-
-   if [[ -z $2 ]]; then 
-     echo "Usage: $0 {user} {path}"
-     return 1
-   fi
-   
-   sudo su $1 -c "/usr/bin/php -f $2"
-}
-
-# Web Services
-function status-code {
-    curl -s -o /dev/null -I -w "%{http_code}" "$1"
 }
 
 # Send notification to pushover
@@ -110,3 +59,14 @@ function push-msg {
 function name_terminal {
     echo -ne "\033]0;$1\007"
 }
+
+# Copy things to the clipboard
+function xcopy {
+    if [ $(which xclip > /dev/null) ]; then
+        echo "You'll need to install xclip";
+        return;
+    fi;
+
+    xclip -selection clipboard
+}
+
